4d17c69bd9393fd8125cf69266949220e7a5425a
ZenZuki <99848533+ZenZuki@users.noreply.github.com>
2024-04-07 18:34:24 +1200

    connected the start menu to the calorie counter, replaced code from startmenu to make it "better" New Day option works, kinda (doesnt make a new day but takes u to the caloriecounter screen

--- caloriecounter/src/main/java/confluence/caloriecounter/Caloriecounter.java
+++ caloriecounter/src/main/java/confluence/caloriecounter/Caloriecounter.java
@@ -12,11 +12,12 @@ import java.util.Scanner;
 
 public class Caloriecounter {
 
-   public static void main(String[] args) {
+   //public static void main(String[] args) { <-- had to remove this so i can call it from the startmenu main
+    public void FoodReader(){
         FoodDatabase macroDatabase = new FoodDatabase();
         
         System.out.println("What did you have today?");
-        System.out.println("(add servings and each item in the format - \"grams\" \"item\". separate each item by a \",\". press enter or return when done)");
+        System.out.println("Add servings and each item in the format \n- \"grams\" \"item\". \nseparate each item by a \",\". \npress enter or return when done)");
         String foodName;
         Scanner scanner = new Scanner(System.in);
         foodName = scanner.nextLine();
@@ -28,6 +29,7 @@ public class Caloriecounter {
             System.out.println("Food: " + foodName);
             System.out.println("Protein: " + foodInfo.getProtein());
             System.out.println("Carbohydrates: " + foodInfo.getCarbs());
+            System.out.println("Calories: " + foodInfo.getCalories());
         } else {
             System.out.println("Food not found in the database. Would you like to add an item?");
         }
--- caloriecounter/src/main/java/confluence/caloriecounter/StartMenu.java
+++ caloriecounter/src/main/java/confluence/caloriecounter/StartMenu.java
@@ -11,18 +11,22 @@ package confluence.caloriecounter;
 import java.util.Scanner;
 
 public class StartMenu {
+    
+        Scanner menuScanner = new Scanner(System.in);
+        Scanner foodScanner = new Scanner(System.in);
+        String choice;
+
+        //FoodDatabase foodDatabase = new FoodDatabase();
+        Caloriecounter inputFood = new Caloriecounter();
+        
     public static void main(String[] args) {
         StartMenu startMenu = new StartMenu();
         startMenu.displayMenu();
+        
+        
     }
 
     public void displayMenu() {
-        Scanner scanner = new Scanner(System.in);
-        String choice;
-
-        FoodDatabase foodDatabase = new FoodDatabase();
-        
-        do {
             System.out.println("Hello, today is day of Macro logging");
             System.out.println("What would you like to do?:");
             System.out.println("- start a new day (nd)");
@@ -32,37 +36,72 @@ public class StartMenu {
             System.out.println("- edit existing meal sets (ms)");
             System.out.println(" exit the program (x)");
             System.out.println("Enter here:");
-            choice = scanner.nextLine();
+            choice = menuScanner.nextLine();
+            
+// |this section has been commented it out cuz i replaced it with the if statements thing below|
+//            switch (choice) {
+//                case "nd":
+//                    System.out.println("You are entering food for day #.");
+//                    System.out.println("What did you have today?:");
+//                    menuScanner.nextLine();
+//                    //FoodItemInfo eggMacros = foodDatabase.getFoodItemInfo("Egg");
+//                    //System.out.println("Egg");
+//                    break;
+//                case "cd":
+//                    System.out.println("Continuing the day...");
+//                    break;
+//                case "ct":
+//                    System.out.println("Your current target is #, what you like your new target to be?");
+//                    break;
+//                case "mq":
+//                    System.out.println("You must look within yourself to save yourself from your other self,");
+//                    System.out.println("only then will your true self reveal itself.");
+//                    break;
+//                case "ms":
+//                    System.out.println("Would you like to edit your breakfast, lunch or dinner?");
+//                    break;
+//                case "x":
+//                case "exit":
+//                    choice = "exit";
+//                    break;
+//                default:
+//                    System.out.println("Invalid choice. Please enter a valid option.");
+//            }
+//        } while (!choice.equals("exit"));
+// |this section has been commented it out cuz i replaced it with the if statements thing below|
 
-            switch (choice) {
-                case "nd":
+
+                if(choice.equals("nd")) {
                     System.out.println("You are entering food for day #.");
+                    inputFood.FoodReader();
+                    //need to run Calorie counter or macroDatabase(); from here but idk how
                     System.out.println("What did you have today?:");
+                    foodScanner.nextLine();
                     //FoodItemInfo eggMacros = foodDatabase.getFoodItemInfo("Egg");
                     //System.out.println("Egg");
-                    break;
-                case "cd":
+                    
+                }
+                if(choice.equals("cd")) {
                     System.out.println("Continuing the day...");
-                    break;
-                case "ct":
+                }
+                if(choice.equals("ct")) {
                     System.out.println("Your current target is #, what you like your new target to be?");
-                    break;
-                case "mq":
+                }
+                if(choice.equals("mq")) {
                     System.out.println("You must look within yourself to save yourself from your other self,");
                     System.out.println("only then will your true self reveal itself.");
-                    break;
-                case "ms":
+                }
+                if(choice.equals("ms")) {
                     System.out.println("Would you like to edit your breakfast, lunch or dinner?");
-                    break;
-                case "x":
-                case "exit":
+                }
+                if(choice.equals("exit") || choice.equals("x")) {
                     choice = "exit";
-                    break;
-                default:
+                }
+                else{
                     System.out.println("Invalid choice. Please enter a valid option.");
+                }
             }
-        } while (!choice.equals("exit"));
 
-        scanner.close();
+//        menuScanner.close();
+//        foodScanner.close();
     }
-}
